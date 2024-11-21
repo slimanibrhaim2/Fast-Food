a96fcf0ad9b02bb5ba049cf18a7ef8c83fe97a07
sliman.ibrahim <sliman.ibrahim@hiast.edu.sy>
2024-11-21 06:43:01 +0300

    init commit

--- FastFood.Repositry/obj/Debug/net8.0/FastFood.Repositry.AssemblyInfo.cs
+++ FastFood.Repositry/obj/Debug/net8.0/FastFood.Repositry.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FastFood.Repositry")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94ea629cc1415d863b33d67ab8da8b49f1c0e687")]
 [assembly: System.Reflection.AssemblyProductAttribute("FastFood.Repositry")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FastFood.Repositry")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
